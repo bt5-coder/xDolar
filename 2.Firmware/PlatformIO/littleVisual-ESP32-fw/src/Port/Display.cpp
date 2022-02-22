88c6b417932b45a8485aca986b89838f4e36f70c
kqian0327 <qiankun0327@126.com>
2022-02-22 17:14:45 +0800

    not need a large RAM cache

--- 2.Firmware/PlatformIO/littleVisual-ESP32-fw/src/Port/Display.cpp
+++ 2.Firmware/PlatformIO/littleVisual-ESP32-fw/src/Port/Display.cpp
@@ -61,7 +61,7 @@ void Port_Init()
     xTaskCreate(
         TaskLvglUpdate,
         "LvglThread",
-        20000,
+        10000,
         nullptr,
         configMAX_PRIORITIES - 1,
         &handleTaskLvgl);
